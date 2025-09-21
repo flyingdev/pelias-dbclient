84c79374f9b8f7b52e5abc00224ace0b53e001b3
flyingdev <developinghuman1015@gmail.com>
2025-09-21 09:35:01 +0800

    switch between open search and elasticsearch

--- src/client.js
+++ src/client.js
@@ -1,6 +1,19 @@
 const elasticsearch = require('elasticsearch');
-const settings = require('pelias-config').generate();
 
 module.exports = function(){
   return new elasticsearch.Client( settings.esclient || {} );
 };
+
+const { Client: ESClient } = require('@elastic/elasticsearch');
+const { Client: OSClient } = require('@opensearch-project/opensearch');
+const settings = require('pelias-config').generate();
+
+// Switch based on config or environment
+let client;
+if (process.env.PELIAS_OPENSEARCH === 'true') {
+  client = new OSClient(settings.esclient || {});
+} else {
+  client = new ESClient( settings.esclient || {} );
+}
+
+module.exports = client;
