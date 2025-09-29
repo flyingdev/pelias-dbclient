7fcc38bffaddaff89cc6f9c3d3c733ceb5270f48
flyingdev <developinghuman1015@gmail.com>
2025-09-29 02:11:27 -0600

    fix: duo client approach fix

--- src/client.js
+++ src/client.js
@@ -1,19 +1,25 @@
-const elasticsearch = require('elasticsearch');
-
-module.exports = function(){
-  return new elasticsearch.Client( settings.esclient || {} );
-};
-
 const { Client: ESClient } = require('@elastic/elasticsearch');
 const { Client: OSClient } = require('@opensearch-project/opensearch');
 const settings = require('pelias-config').generate();
 
-// Switch based on config or environment
-let client;
-if (process.env.PELIAS_OPENSEARCH === 'true') {
-  client = new OSClient(settings.esclient || {});
-} else {
-  client = new ESClient( settings.esclient || {} );
-}
+module.exports = function() {
+  if (process.env.PELIAS_OPENSEARCH === 'true') {
+    // Prefer explicit env var
+    let node = process.env.OPENSEARCH_NODE;
 
-module.exports = client;
+    // Fallback: build from pelias config
+    if (!node && settings.esclient?.hosts?.[0]) {
+      const { protocol, host, port } = settings.esclient.hosts[0];
+      node = `${protocol}://${host}:${port}`;
+    }
+
+    if (!node) {
+      throw new Error('No OpenSearch node URL found. Please set OPENSEARCH_NODE or configure esclient.hosts.');
+    }
+
+    return new OSClient({ node });
+  }
+
+  // Default: Elasticsearch client
+  return new ESClient(settings.esclient || {});
+};
